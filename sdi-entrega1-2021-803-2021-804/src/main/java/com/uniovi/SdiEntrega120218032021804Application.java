83d95cf882eb2b6ddaf18987f79faca8ae1b9f3b
UO271411 <UO271411@uniovi.es>
2021-03-07 17:41:25 +0100

    SDI-2021-803-2021-804-Creando proyecto.

---
+++ sdi-entrega1-2021-803-2021-804/src/main/java/com/uniovi/SdiEntrega120218032021804Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega120218032021804Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega120218032021804Application.class, args);
+	}
+
+}
