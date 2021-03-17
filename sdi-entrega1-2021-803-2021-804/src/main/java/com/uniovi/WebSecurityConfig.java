06e18a8027f2ffd2e5c6928769aeacc8a6afb87c
uo269736 <uo269736@uniovi.es>
2021-03-17 23:57:41 +0100

    SDI-2021-803-2021-804-Vista home y nav

--- sdi-entrega1-2021-803-2021-804/src/main/java/com/uniovi/WebSecurityConfig.java
+++ sdi-entrega1-2021-803-2021-804/src/main/java/com/uniovi/WebSecurityConfig.java
@@ -26,16 +26,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().disable().authorizeRequests()
-//				.antMatchers("/css/**", "/img/**", "/script/**", "/", "/signup", "/login/**").permitAll()
-//				.antMatchers("/mark/add").hasAuthority("ROLE_PROFESSOR").antMatchers("/mark/edit/*")
-//				.hasAuthority("ROLE_PROFESSOR").antMatchers("/mark/delete/*").hasAuthority("ROLE_PROFESSOR")
-//				.antMatchers("/mark/**").hasAnyAuthority("ROLE_STUDENT", "ROLE_PROFESSOR", "ROLE_ADMIN")
-//				.antMatchers("/professor/list").hasAnyAuthority("ROLE_STUDENT", "ROLE_PROFESSOR", "ROLE_ADMIN")
-//				.antMatchers("/professor/details/**").hasAnyAuthority("ROLE_PROFESSOR", "ROLE_ADMIN")
-//				.antMatchers("/professor/**").hasAnyAuthority("ROLE_ADMIN")
-//				.antMatchers("/user/**").hasAnyAuthority("ROLE_ADMIN").anyRequest().authenticated().and().formLogin()
-//				.loginPage("/login").permitAll().defaultSuccessUrl("/home").and().logout().permitAll();
+		http.csrf().disable().authorizeRequests()
+				.antMatchers("/css/**", "/img/**", "/script/**", "/", "/signup", "/login/**").permitAll()
+				.antMatchers("/oferta/add").hasAuthority("ROLE_USUARIO")
+				.antMatchers("/oferta/delete/*").hasAnyAuthority("ROLE_USUARIO","ROLE_ADMIN")
+				.antMatchers("/user/list/*").hasAnyAuthority("ROLE_ADMIN")
+				.antMatchers("/user/delete/*").hasAnyAuthority("ROLE_ADMIN")
+				.anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").permitAll().defaultSuccessUrl("/home").and().logout().permitAll();
 	}
 
 	@Autowired
--- sdi-entrega1-2021-803-2021-804/src/main/java/com/uniovi/controllers/UserController.java
+++ sdi-entrega1-2021-803-2021-804/src/main/java/com/uniovi/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -77,6 +79,11 @@ public class UserController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
 		return "login";
+	}
+	
+	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	public String home(Model model) {
+		return "home";
 	}
 
 }
