37834a441e6e4324121fa9171dcc1554a42e2a47
uo269736 <uo269736@uniovi.es>
2021-03-19 20:59:22 +0100

    SDI-2021-803-2021-804-Añadir ofertas y preparación test.

--- sdi-entrega1-2021-803-2021-804/src/test/java/com/uniovi/SdiEntrega120218032021804ApplicationTests.java
+++ sdi-entrega1-2021-803-2021-804/src/test/java/com/uniovi/SdiEntrega120218032021804ApplicationTests.java
@@ -19,15 +19,15 @@ public class SdiEntrega120218032021804ApplicationTests {
 
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas)):
-	
-	//Miguel
+
+	// Miguel
 	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver024 = "C:\\Users\\MiguelUni\\Desktop\\TrabajoUniversidadMiguel\\Tercero\\SDI\\Sesion 5\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
-	
-	//Alex
+
+	// Alex
 //	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 //  static String Geckdriver024 = "C:\\Users\\MiguelUni\\Desktop\\TrabajoUniversidadMiguel\\Tercero\\SDI\\Sesion 5\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
-	
+
 	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
 	static String URL = "http://localhost:8090";
 
@@ -66,63 +66,209 @@ public class SdiEntrega120218032021804ApplicationTests {
 	void contextLoads() {
 	}
 
-//	// Prueba1 Registro de profesores con datos válidos
-//	@Test
-//	public void Prueba1() {
-//		// Hacemos login como administrador
-//		PO_PrivateView.loginAndCheck(driver, "99999988F", "123456", "Notas del usuario");
-//		// Seleccionamos gestión de profesores
-//		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'professor-menu')]/a");
-//		elementos.get(0).click();
-//		// Seleccionamos la opción agregar profesores
-//		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'professor/add')]");
-//		elementos.get(0).click();
-//		// Rellenamos el cuestionario
-//		PO_ProfessorView.fillForm(driver, "12345678G", "Maria", "Fernandez Alvarez", "Ciencias");
-//		// Comprobamos que lo rellenamos con exito y que cambiamos de pantalla
-//		PO_View.checkElement(driver, "text", "Profesores");
-//	}
-//
-//	// Prueba2 Registro de profesores con datos inválidos (nombre y categoría
-//	// inválidos)
-//	@Test
-//	public void Prueba2() {
-//		// Hacemos login como administrador
-//		PO_PrivateView.loginAndCheck(driver, "99999988F", "123456", "Notas del usuario");
-//		// Seleccionamos gestión de profesores
-//		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'professor-menu')]/a");
-//		elementos.get(0).click();
-//		// Seleccionamos la opción agregar profesores
-//		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'professor/add')]");
-//		elementos.get(0).click();
-//		// Rellenamos el cuestionario con un nombre demasiado corto
-//		PO_ProfessorView.fillForm(driver, "12345678G", "M", "Fernandez Alvarez", "Ciencias");
-//		// Comprobamos que nos da error de nombre corto
-//		PO_RegisterView.checkKey(driver, "Error.signup.name.length", PO_Properties.getSPANISH());
-//		// Rellenamos el cuestionario con una categoria demasiado corta
-//		PO_ProfessorView.fillForm(driver, "22222222G", "Maria", "Fernandez Alvarez", "C");
-//		// Comprobamos que nos da error de categoria corta
-//		PO_RegisterView.checkKey(driver, "Error.profesor.agregar.categoria", PO_Properties.getSPANISH());
-//
-//	}
-//
-//	// Prueba3 Verificar que solo los usuarios autorizados pueden dar de alta un
-//	// profesor.
-//	@Test
-//	public void Prueba3() {
-//		// Hacemos login como alumno
-//		PO_PrivateView.loginAndCheck(driver, "99999990A", "123456", "Notas del usuario");
-//		// Seleccionamos gestión de profesores
-//		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'professor-menu')]/a");
-//		elementos.get(0).click();
-//		// Comprobamos que no nos sale agregar profesores
-//		SeleniumUtils.textoNoPresentePagina(driver, "Agregar Profesor");
-//		// Intentamos acceder a agregar profesores directamente
-//		driver.navigate().to("http://localhost:8090/professor/add");
-//		// Comprobamos que aparece el texto que nos prohibe entrar
-//		SeleniumUtils.textoPresentePagina(driver, "HTTP Status 403 – Forbidden");
-//	}
-//	
-	
+	// [Prueba1] Registro de Usuario con datos válidos.
+	@Test
+	public void Prueba1() {
+
+	}
+
+	// [Prueba2] Registro de Usuario con datos inválidos (email vacío, nombre vacío, apellidos vacíos).
+	@Test
+	public void Prueba2() {
+
+	}
+
+	// [Prueba3] Registro de Usuario con datos inválidos (repetición de contraseña inválida).
+	@Test
+	public void Prueba3() {
+
+	}
+
+	// [Prueba4] Registro de Usuario con datos inválidos (email existente).
+	@Test
+	public void Prueba4() {
+
+	}
+
+	// [Prueba5] Inicio de sesión con datos válidos (administrador).
+	@Test
+	public void Prueba5() {
+
+	}
+
+	// [Prueba6] Inicio de sesión con datos válidos (usuario estándar).
+	@Test
+	public void Prueba6() {
+
+	}
+
+	// [Prueba7] Inicio de sesión con datos inválidos (usuario estándar, campo email y contraseña vacíos).
+	@Test
+	public void Prueba7() {
+
+	}
+
+	// [Prueba8] Inicio de sesión con datos válidos (usuario estándar, email existente, pero contraseña
+	// incorrecta).
+	@Test
+	public void Prueba8() {
+
+	}
+
+	// [Prueba9] Inicio de sesión con datos inválidos (usuario estándar, email no existente en la aplicación).
+	@Test
+	public void Prueba9() {
+
+	}
+
+	// [Prueba10] Hacer click en la opción de salir de sesión y comprobar que se redirige a la página de inicio
+	// de sesión (Login).
+	@Test
+	public void Prueba10() {
+
+	}
+
+	// [Prueba11] Comprobar que el botón cerrar sesión no está visible si el usuario no está autenticado
+	@Test
+	public void Prueba11() {
+
+	}
+
+	// [Prueba12] Mostrar el listado de usuarios y comprobar que se muestran todos los que existen en el
+	// sistema. 
+	@Test
+	public void Prueba12() {
+
+	}
+
+	// [Prueba13] Ir a la lista de usuarios, borrar el primer usuario de la lista, comprobar que la lista se actualiza
+	// y que el usuario desaparece.
+	@Test
+	public void Prueba13() {
+
+	}
+
+	// [Prueba14] Ir a la lista de usuarios, borrar el último usuario de la lista, comprobar que la lista se actualiza
+	// y que el usuario desaparece.
+	@Test
+	public void Prueba14() {
+
+	}
+
+	// [Prueba15] Ir a la lista de usuarios, borrar 3 usuarios, comprobar que la lista se actualiza y que los
+	// usuarios desaparecen
+	@Test
+	public void Prueba15() {
+
+	}
+
+	// [Prueba16] Ir al formulario de alta de oferta, rellenarla con datos válidos y pulsar el botón Submit.
+	// Comprobar que la oferta sale en el listado de ofertas de dicho usuario.
+	@Test
+	public void Prueba16() {
+
+	}
+
+	// [Prueba17] Ir al formulario de alta de oferta, rellenarla con datos inválidos (campo título vacío) y pulsar
+	// el botón Submit. Comprobar que se muestra el mensaje de campo obligatorio.
+	@Test
+	public void Prueba17() {
+
+	}
+
+	// [Prueba18] Mostrar el listado de ofertas para dicho usuario y comprobar que se muestran todas los que
+	// existen para este usuario. 
+	@Test
+	public void Prueba18() {
+
+	}
+
+	// [Prueba19] Ir a la lista de ofertas, borrar la primera oferta de la lista, comprobar que la lista se actualiza y
+	// que la oferta desaparece.
+	@Test
+	public void Prueba19() {
+
+	}
+
+	// Prueba20] Ir a la lista de ofertas, borrar la última oferta de la lista, comprobar que la lista se actualiza y
+	// que la oferta desaparece.
+	@Test
+	public void Prueba20() {
+
+	}
+
+	// [Prueba21] Hacer una búsqueda con el campo vacío y comprobar que se muestra la página que
+	// corresponde con el listado de las ofertas existentes en el sistema
+	@Test
+	public void Prueba21() {
+
+	}
+
+	// [Prueba22] Hacer una búsqueda escribiendo en el campo un texto que no exista y comprobar que se
+	// muestra la página que corresponde, con la lista de ofertas vacía.
+	@Test
+	public void Prueba22() {
+
+	}
+
+	// Prueba23] Sobre una búsqueda determinada (a elección del desarrollador), comprar una oferta que deja
+	// un saldo positivo en el contador del comprador. Comprobar que el contador se actualiza correctamente
+	// en la vista del comprador
+	@Test
+	public void Prueba23() {
+
+	}
+
+	// [Prueba24] Sobre una búsqueda determinada (a elección del desarrollador), comprar una oferta que deja
+	// un saldo 0 en el contador del comprador. Comprobar que el contador se actualiza correctamente en la
+	// vista del comprador. 
+	@Test
+	public void Prueba24() {
+
+	}
+
+	// [Prueba25] Sobre una búsqueda determinada (a elección del desarrollador), intentar comprar una oferta
+	// que esté por encima de saldo disponible del comprador. Y comprobar que se muestra el mensaje de
+	// saldo no suficiente.
+	@Test
+	public void Prueba25() {
+
+	}
+
+	// [Prueba26] Ir a la opción de ofertas compradas del usuario y mostrar la lista. Comprobar que aparecen
+	// las ofertas que deben aparecer.
+	@Test
+	public void Prueba26() {
+
+	}
+
+	// [Prueba27] Visualizar al menos cuatro páginas haciendo el cambio español/inglés/español
+	// (comprobando que algunas de las etiquetas cambian al idioma correspondiente). Página
+	// principal/Opciones principales de usuario/Listado de usuarios /Vista de alta de oferta.
+	@Test
+	public void Prueba27() {
+
+	}
+
+	// [Prueba28] Intentar acceder sin estar autenticado a la opción de listado de usuarios del administrador. Se
+	// deberá volver al formulario de login.
+	@Test
+	public void Prueba28() {
+
+	}
+
+	// [Prueba29] Intentar acceder sin estar autenticado a la opción de listado de ofertas propias de un usuario
+	// estándar. Se deberá volver al formulario de login.
+	@Test
+	public void Prueba29() {
+
+	}
+
+	// [Prueba30] Estando autenticado como usuario estándar intentar acceder a la opción de listado de
+	// usuarios del administrador. Se deberá indicar un mensaje de acción prohibida.
+	@Test
+	public void Prueba30() {
+
+	}
 
 }
