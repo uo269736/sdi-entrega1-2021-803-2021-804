7e051b982d6e4972aeec0be5de42feecb647ab02
uo269736 <uo269736@uniovi.es>
2021-03-21 16:16:57 +0100

    SDI-2021-803-2021-804-Tests de el ejercicio opcional de destacar

--- sdi-entrega1-2021-803-2021-804/src/test/java/com/uniovi/tests/pageobjects/PO_OfertaAddView.java
+++ sdi-entrega1-2021-803-2021-804/src/test/java/com/uniovi/tests/pageobjects/PO_OfertaAddView.java
@@ -27,6 +27,27 @@ public class PO_OfertaAddView extends PO_NavView{
 		driver.findElement(boton).click();
 	}
 	
+	static public void fillForm(WebDriver driver, String ptitulo, String pdescripcion, String pcantidad, boolean destacada) {
+		WebElement titulo = driver.findElement(By.name("titulo"));
+		titulo.click();
+		titulo.clear();
+		titulo.sendKeys(ptitulo);
+		WebElement descripcion = driver.findElement(By.name("descripcion"));
+		descripcion.click();
+		descripcion.clear();
+		descripcion.sendKeys(pdescripcion);		
+		WebElement cantidad = driver.findElement(By.name("cantidad"));
+		cantidad.click();
+		cantidad.clear();
+		cantidad.sendKeys(pcantidad);	
+		//Seleccionamos el checkbok
+		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//input[contains(@type,'checkbox')]");
+		elementos.get(0).click();
+		// Pulsar el boton de Enviar.
+		By boton = By.className("btn");
+		driver.findElement(boton).click();
+	}
+	
 	static public void creaOferta(WebDriver driver, String titulo, String descripcion, double cantidad) {
 		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'ofertas-menu')]/a");
 		elementos.get(0).click();
@@ -35,6 +56,16 @@ public class PO_OfertaAddView extends PO_NavView{
 		elementos.get(0).click();
 		// Rellenamos el formulario y lo enviamos
 		PO_OfertaAddView.fillForm(driver, titulo, descripcion, String.valueOf(cantidad));
+	}
+	
+	static public void creaOferta(WebDriver driver, String titulo, String descripcion, double cantidad, boolean destacada) {
+		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'ofertas-menu')]/a");
+		elementos.get(0).click();
+		// Accedemos a Agregar Ofertas
+		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'oferta/add')]");
+		elementos.get(0).click();
+		// Rellenamos el formulario y lo enviamos
+		PO_OfertaAddView.fillForm(driver, titulo, descripcion, String.valueOf(cantidad), true);
 	}
 	
 	static public void checkChangeIdiom(WebDriver driver, String textIdiom1, String textIdiom2, int locale1,
